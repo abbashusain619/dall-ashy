fd529bf3f6afc1d49f76aee692ed08666d23c0a9
abbashusain619 <abbashusain1996@gmail.com>
2023-02-28 11:15:06 +0300

    first commit

---
+++ client/src/assets/index.js
@@ -0,0 +1,11 @@
+import download from './download.png';
+import logo from './logo.svg';
+import AshyPanda from './AP.png';
+import preview from './preview.png';
+
+export {
+  download,
+  logo,
+  preview,
+  AshyPanda,
+};
